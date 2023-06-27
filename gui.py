4f983cbdb16a5e9249569adf3bd0fbe67a3a09bc
Spencer <spencer.creveling@gmail.com>
2023-06-27 13:12:39 -0400

    Added search functionality

--- gui.py
+++ gui.py
@@ -14,7 +14,6 @@ class modGUI:
         #OPTION FRAME TOP ROW
         self.optionframe = tk.Frame(self.root)
         self.optionframe.grid(row=0, column=0,sticky="nsew")
-
         self.Paths = tk.Button(self.optionframe, text="Paths", height=1,command=self.displayPaths)
         self.Paths.grid(row=0, column=0,sticky="nw")
 
@@ -25,18 +24,25 @@ class modGUI:
         self.listFrame.columnconfigure(0, weight=1)
         self.listFrame.columnconfigure(1, weight=1)
         self.listFrame.grid(row=1, column=0,sticky="nsew")
-        
+
+        #sort button to sort the list either my compatibility or aplpabaticly 
         self.SortButton = tk.Button(self.listFrame, text="Sort Numericaly", height=1,command=self.sortlists)
         self.SortButton.grid(row=0, column=0,sticky="nw")
-
         self.alphabeticSort = True
 
+        #search nar to search for mods
+        self.SearchBar = tk.Entry(self.listFrame, relief="sunken",)
+        self.SearchBar.grid(row=0, column=1,sticky="nsew")
+        self.SearchBar.bind("<KeyRelease>", self.filter)
+
+        #unloaded mod list
         self.Modlist = tk.Listbox(self.listFrame)
         self.mods = indexSteamMods(getPaths()[0])
         self.mods = updateLoadedMods(self.mods)
         self.updateAllModList()
         self.Modlist.grid(row=1, column=0,sticky="nsew")
 
+        #loaded mod list
         self.ActiveModlist = tk.Listbox(self.listFrame)
         self.mods = indexSteamMods(getPaths()[0])
         self.mods = updateLoadedMods(self.mods)
@@ -50,7 +56,7 @@ class modGUI:
         self.root.mainloop()
 
     def displayPaths(self):
-
+        #path pop up to let user specifie install diretories
         self.PathPopup = tk.Tk()
         self.PathPopup.title("Paths")
         self.PathPopup.geometry("600x100")
@@ -62,12 +68,12 @@ class modGUI:
         self.PathPopup.columnconfigure(0, weight=0)
         self.PathPopup.columnconfigure(1, weight=4)
         self.PathPopup.columnconfigure(2, weight=0)
-        
+        #get paths from file and populate the lables
         self.paths = getPaths()
         self.steamModsDir = tk.StringVar(self.PathPopup, value= self.paths[0])
         self.configDIR = tk.StringVar(self.PathPopup, value= self.paths[1])
         self.rimworldDir = tk.StringVar(self.PathPopup, value= self.paths[2])
-
+        #the labels themselves
         steamLabel = tk.Label(self.PathPopup, text="Steam Mods Directory",font=("Arial", 8))
         configLabel = tk.Label(self.PathPopup, text="Config Directory",font=("Arial", 8))
         rimworldLabel = tk.Label(self.PathPopup, text="RimWorld Directory",font=("Arial", 8))
@@ -127,29 +133,51 @@ class modGUI:
             self.mods.sort(key=lambda x: x.name)
             for mod in self.mods:
                 if(not mod.loaded):
-                    self.Modlist.insert(tk.END, mod)
-                    self.Modlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    if(not self.SearchBar.get() == "" and self.SearchBar.get().lower() in mod.name.lower()):
+                        self.Modlist.insert(tk.END, mod)
+                        self.Modlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    elif(self.SearchBar.get() == ""):
+                        self.Modlist.insert(tk.END, mod)
+                        self.Modlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
         else:
             self.mods.sort(key=lambda x: x.compatibility)
             for mod in self.mods:
                 if(not mod.loaded):
-                    self.Modlist.insert(0, mod)
-                    self.Modlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
-
+                    if(not self.SearchBar.get() == "" and self.SearchBar.get().lower() in mod.name.lower()):
+                        self.Modlist.insert(0, mod)
+                        self.Modlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    elif(self.SearchBar.get() == ""):
+                        self.Modlist.insert(0, mod)
+                        self.Modlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+    
     def updateActiveModList(self):
         self.ActiveModlist.delete(0, tk.END)
         if(self.alphabeticSort):
             self.mods.sort(key=lambda x: x.name)
             for mod in self.mods:
                 if(mod.loaded):
-                    self.ActiveModlist.insert(tk.END, mod)
-                    self.ActiveModlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    if(not self.SearchBar.get() == "" and self.SearchBar.get().lower() in mod.name.lower()):
+                        self.ActiveModlist.insert(tk.END, mod)
+                        self.ActiveModlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    elif(self.SearchBar.get() == ""):
+                        self.ActiveModlist.insert(tk.END, mod)
+                        self.ActiveModlist.itemconfig(tk.END, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
         else:
             self.mods.sort(key=lambda x: x.compatibility)
             for mod in self.mods:
                 if(mod.loaded):
-                    self.ActiveModlist.insert(0, mod)
-                    self.ActiveModlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    if(not self.SearchBar.get() == "" and self.SearchBar.get().lower() in mod.name.lower()):
+                        self.ActiveModlist.insert(0, mod)
+                        self.ActiveModlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+                    elif(self.SearchBar.get() == ""):
+                        self.ActiveModlist.insert(0, mod)
+                        self.ActiveModlist.itemconfig(0, bg="red" if mod.compatibility == 1 else "orange" if mod.compatibility == 2 else "yellow" if mod.compatibility == 3 else "green" if mod.compatibility == 4 else "gray")
+    
+    def filter(self,e):
+        self.updateAllModList()
+        self.updateActiveModList()
+        
+        
         
 
 
